f1904821cef7a90161170c4b13f68ceb50690f45
PEDSF1968 <pe.dsf@free.fr>
2019-05-20 17:53:54 +0200

    Microservice clientui package beans - ajout du bean PaiementBean pour payer une commande
    package proxies
    - ajout du proxy pour payer les commandes
    
    package template
    - ajout vue paiement
    - ajout vue confirmation
    
    package controller
    - ajout méthode paserCommande
    - ajout méthode payerCommande
    
    Microservice mpaiement
    package beans
    - ajout du bean pour les commandes
    
    package proxies
    - ajout du proxy pour payer les commandes

--- clientui/src/main/java/com/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/clientui/controller/ClientController.java
@@ -1,22 +1,34 @@
 package com.clientui.controller;
 
+import com.clientui.beans.CommandeBean;
+import com.clientui.beans.PaiementBean;
 import com.clientui.beans.ProductBean;
+import com.clientui.proxies.MicroserviceCommandeProxy;
+import com.clientui.proxies.MicroservicePaiementProxy;
 import com.clientui.proxies.MicroserviceProduitsProxy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 
 @Controller
 public class ClientController {
 
     @Autowired
-    private MicroserviceProduitsProxy ProduitsProxy;
+    private MicroserviceProduitsProxy mProduitsProxy;
 
+    @Autowired
+    private MicroserviceCommandeProxy mCommandesProxy;
+
+    @Autowired
+    private MicroservicePaiementProxy mPaiementProxy;
 
 
     /*
@@ -28,7 +40,7 @@ public class ClientController {
     @RequestMapping("/")
     public String accueil(Model model){
 
-        List<ProductBean> produits =  ProduitsProxy.listeDesProduits();
+        List<ProductBean> produits =  mProduitsProxy.listeDesProduits();
 
         model.addAttribute("produits", produits);
 
@@ -43,10 +55,65 @@ public class ClientController {
     @RequestMapping("/details-produit/{id}")
     public String ficheProduit(@PathVariable int id,  Model model){
 
-        ProductBean produit = ProduitsProxy.recupererUnProduit(id);
+        ProductBean produit = mProduitsProxy.recupererUnProduit(id);
 
         model.addAttribute("produit", produit);
 
         return "FicheProduit";
+    }
+
+    /**
+     * Étape (3) et (4)
+     * Opération qui fait appel au microservice de commande pour placer une commande et récupérer les détails de la commande créée
+     **/
+    @RequestMapping("/commander-produit/{idProduit}/{montant}")
+    public String passerCommande(@PathVariable int idProduit, @PathVariable Double montant, Model model) {
+
+        CommandeBean commande = new CommandeBean();
+
+        //On renseigne les propriétés de l'objet de type CommandeBean que nous avons crée
+        commande.setProductId(idProduit);
+        commande.setQuantite(1);
+        commande.setDateCommande(new Date());
+
+        //appel du microservice commandes grâce à Feign et on récupère en retour les détails de la commande créée, notamment son ID (étape 4).
+
+        CommandeBean commandeAjoutee = mCommandesProxy.ajouterCommande(commande);
+
+        //on passe à la vue l'objet commande et le montant de celle-ci afin d'avoir les informations nécessaire pour le paiement
+        model.addAttribute("commande", commandeAjoutee);
+        model.addAttribute("montant", montant);
+
+        return "Paiement";
+    }
+
+    /**
+     * Étape (5)
+     * Opération qui fait appel au microservice de paiement pour traiter un paiement
+     **/
+    @RequestMapping("/payer-commande/{idCommande}/{montant}")
+    public String payerCommande(@PathVariable int idCommande, @PathVariable Double montant, Model model){
+
+        PaiementBean paiementAExecuter = new PaiementBean();
+
+        paiementAExecuter.setIdCommande(idCommande);
+        paiementAExecuter.setMontant(montant);
+        paiementAExecuter.setNumeroCarte(numcarte());
+
+        ResponseEntity<PaiementBean> paiement = mPaiementProxy.payerUneCommande(paiementAExecuter);
+
+        Boolean paiementAccepte = false;
+        //si le code est autre que 201 CREATED, c'est que le paiement n'a pas pu aboutir.
+        if(paiement.getStatusCode() == HttpStatus.CREATED)
+            paiementAccepte = true;
+
+        model.addAttribute("paiementOK", paiementAccepte);
+
+        return "Confirmation";
+    }
+
+    //Génére une serie de 16 chiffres au hasard pour simuler vaguement une CB
+    private Long numcarte() {
+        return ThreadLocalRandom.current().nextLong(1000000000000000L, 9000000000000000L);
     }
 }
