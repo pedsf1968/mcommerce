225408c2cb354ea1485abe2561f735ce78524a94
PEDSF1968 <pe.dsf@free.fr>
2019-05-21 19:31:57 +0200

    Ajout du @RefreshScope pour le bean de configuration de produit et des dépendances ainsi que la ligne de configuration dans le bootstrap

--- microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
+++ microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
@@ -2,8 +2,10 @@ package com.mcommandes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.mcommandes")
 public class McommandesApplication {
 
 	public static void main(String[] args) {
--- microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
+++ microservice-produits/src/main/java/com/mproduits/MproduitsApplication.java
@@ -2,8 +2,10 @@ package com.mproduits;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.mproduits")
 public class MproduitsApplication {
 
 	public static void main(String[] args) {
