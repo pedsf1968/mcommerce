1a8062bf3e3d6f15c8786b82a18cca7441e2984f
PEDSF1968 <pe.dsf@free.fr>
2019-05-21 09:52:16 +0200

    Correction du bug de démmarage de paiement

--- clientui/src/main/java/com/clientui/proxies/MicroserviceCommandeProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceCommandeProxy.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name="microservice-commande", url="localhost:9002")
+@FeignClient(name="microservice-commandes", url="localhost:9002")
 public interface MicroserviceCommandeProxy {
 
     @PostMapping(value="/commandes")
