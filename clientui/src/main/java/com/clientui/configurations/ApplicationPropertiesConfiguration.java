7f6ecc57377790b09372a53d860ffce2dcc8b20a
PEDSF1968 <pe.dsf@free.fr>
2019-05-21 18:44:57 +0200

    changement de nom du fichier

---
+++ clientui/src/main/java/com/clientui/configurations/ApplicationPropertiesConfiguration.java
@@ -0,0 +1,12 @@
+package com.clientui.configurations;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("mes-configs")
+@RefreshScope
+public class ApplicationPropertiesConfiguration {
+
+}
