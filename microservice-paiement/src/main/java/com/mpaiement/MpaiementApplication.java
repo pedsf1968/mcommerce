0fe07029e03b39fdbd02de09b9e65a934212592a
PEDSF1968 <pe.dsf@free.fr>
2019-05-22 08:54:59 +0200

    Ajout du serveur Eureka
    Ajout de l'adresse du serveur Eureka dans les fichiers de configuration GIT
    Ajout de la déclaration @EnableDiscoveryClient dans les Microservices

--- microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -2,9 +2,11 @@ package com.mpaiement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.mpaiement")
 public class MpaiementApplication {
 	public static void main(String[] args) {
