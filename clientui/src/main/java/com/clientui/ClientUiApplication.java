336f005bfb56fb5037cbb8c5bd461c8b9f14d861
PEDSF1968 <pe.dsf@free.fr>
2019-05-22 11:56:08 +0200

    Correction du bug d'Eureka et implémentation de Ribbon

--- clientui/src/main/java/com/clientui/ClientUiApplication.java
+++ clientui/src/main/java/com/clientui/ClientUiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients("com.clientui")
 @EnableDiscoveryClient
+@EnableFeignClients("com.clientui")
 public class ClientUiApplication {
 
     public static void main(String[] args) {
