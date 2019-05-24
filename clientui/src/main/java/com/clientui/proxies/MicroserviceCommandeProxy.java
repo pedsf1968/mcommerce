4d9fa9297e9f3b038ce7b26957814218b07ca959
PEDSF1968 <pe.dsf@free.fr>
2019-05-24 17:55:49 +0200

    suppression du passage par zuul pour les proxy commande et paiement de clientui

--- clientui/src/main/java/com/clientui/proxies/MicroserviceCommandeProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceCommandeProxy.java
@@ -1,16 +1,16 @@
 package com.clientui.proxies;
 
 import com.clientui.beans.CommandeBean;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "zuul-server")
-@RibbonClient(name = "microservice-commandes")
+@FeignClient(name = "microservice-commandes", url = "localhost:9002")
+//@RibbonClient(name = "microservice-commandes")
 public interface MicroserviceCommandeProxy {
 
-    @PostMapping(value = "/microservice-commandes/commandes")
+    @PostMapping(value = "/commandes")
     ResponseEntity<CommandeBean> ajouterCommande(@RequestBody CommandeBean commande);
 }
--- clientui/src/main/java/com/clientui/proxies/MicroservicePaiementProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroservicePaiementProxy.java
@@ -1,16 +1,16 @@
 package com.clientui.proxies;
 
 import com.clientui.beans.PaiementBean;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+//import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "zuul-server")
-@RibbonClient(name = "microservice-paiement")
+@FeignClient(name = "microservice-paiement",url="localhost:9003")
+//@RibbonClient(name = "microservice-paiement")
 public interface MicroservicePaiementProxy {
 
-    @PostMapping(value="/microservice-paiement/paiement")
+    @PostMapping(value="/paiement")
     ResponseEntity<PaiementBean> payerUneCommande(@RequestBody PaiementBean paiement);
 }
